98645f151f531da7e284da494448f3df0e4bf341
clint601 <clintjackson601@gmail.com>
2024-12-12 09:16:35 -0600

    done

--- js/app.js
+++ js/app.js
@@ -1,46 +1,44 @@
 /**
  * Regex
  * 
- * regular expression
+ * Regular Expressions
  * 
- * [a-z] => it matches lowercase letters
- * [A-Z] => it marches uppercase letters
- * [^A-Z] => it matches an non-uppercase letter
- * [^a-z] => it an non-lowercase letter
+ * [a-z] => matches lowercase letters
+ * [A-Z] => matches uppercase letters
+ * [^A-Z] => a non-uppercase letter
+ * [^a-z] => a non-lowercase letter
  * [0-9] => matches any digits
- * [^0-9] => an non-numberical character
+ * [^0-9] => a non-numerical character
  * \d => equivalent to [0-9]
- * \D = equivalent to [^0-9]
- * . => matches any songle charactr excpet line terminators
+ * \D => equivalent to [^0-9]
+ * . => matches any single character except line terminators
  * [A-Za-z] => matches any letter, lowercase or uppercase
  * \w => matches any alphanumeric character [a-zA-Z0-9]
- * \W => this matches any special characters
+ * \W => special characters
  * \s => whitespace
- * \S => check for any characters other than white space
- * $ => match the end of an input
- * ^ match the beginning of an input
+ * \S => any character other than whitespace
  * 
+ * $ => matches the end of an input
+ * ^ => matches the beginning of an input
  */
-// const regex = /\D/ // matches lowercase letter
-// // const regex = /[A-Z]/
 
-// let myString = 'Hello'
+// const regex = /ki/ 
 
-// regex.test(str) => returns true or false
+// let myString = 'I like to eat apples'
 
-// console.log(myString.match(regex))
-// console.log(regex.test(myString)) // test to see if there any lowercase letters
 
-// const regex2 = /s$/
+// regex.test(str) => returns true or false 
+// console.log(regex.test(myString))
 
-// myString = 'I sure do love to cade'
+// const regex2 = /[^I][s$]/
+
+// myString = 'I sure do love to codes'
 
 // if (myString.match(regex2)) {
 //     console.log('match')
 // } else {
 //     console.log('no match')
 // }
-
 
 const confirmBtn = document.getElementById('confirmBtn')
 
@@ -55,15 +53,12 @@ let userObj = {
 let users = []
 
 // verify password
+const verifyPassword =()=> {
+    const username = document.getElementById('username').value
 
-const verifyPassword = ()=> {
-    const username = document.getElementById('username'). value
+    const password = document.getElementById('password').value 
 
-    const password = document.getElementById('password'). value
-
-    // console.log(`username: ${username}; password: ${password}`)
-
-    const lowerTest = /[a-z]/ // test to see if there a lower case letter
+    const lowerTest = /[a-z]/
     const upperTest = /[A-Z]/
     const numTest = /\d/
     const specialTest = /\W/
@@ -75,37 +70,39 @@ const verifyPassword = ()=> {
         const reqItem = document.createElement('li')
         reqItem.innerText = 'Password must be at least 8 characters'
         reqList.appendChild(reqItem)
+    } else {
+        document.getElementById('eightChar').style.color = 'green'
     }
 
-    if (!password.match (lowerTest)) {
+    if (!password.match(lowerTest)) {
         const reqItem = document.createElement('li')
-        reqItem.innerText = 'must contain at least one lowercase letter'
+        reqItem.innerText = 'Must contain at least one lowercase letter'
         reqList.appendChild(reqItem)
     }
 
-    if (!password.match (upperTest)) {
+    if (!password.match(upperTest)) {
         const reqItem = document.createElement('li')
         reqItem.innerText = 'Must contain at least one uppercase letter'
         reqList.appendChild(reqItem)
     }
 
-    if (!password.match (numTest)) {
+    if (!password.match(numTest)) {
         const reqItem = document.createElement('li')
         reqItem.innerText = 'Must contain at least one digit (0-9)'
         reqList.appendChild(reqItem)
     }
 
-    if (!password.match (specialTest)) {
+    if (!password.match(specialTest)) {
         const reqItem = document.createElement('li')
-        reqItem.innerText = 'Must contain at least one special test  letter'
+        reqItem.innerText = 'Must contain at least one special character'
         reqList.appendChild(reqItem)
     }
 
     const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*\W).{8,}$/gm
 
-    username.length == 0 ? alert('Please enter a username'): null
+    username.length == 0 ? alert('Please enter a username') : null 
 
-    // if (!password.match(regex)) {
+    // if (password.match(regex)) {
     //     display.innerText = 'Account created'
     // } else {
     //     display.appendChild(reqList)
@@ -113,34 +110,33 @@ const verifyPassword = ()=> {
 
     password.match(regex) ? createUser(username, password) : display.appendChild(reqList)
 }
-const matchPasswords = ()=> {
-    const matchPassword = document.getElementById('matchPassword'). value
 
-    const confirmPassord = document.getElementById('confirmPassword').value
+const matchPasswords =()=> {
+    const password = document.getElementById('password').value 
+
+    const confirmPassword = document.getElementById('confirmPassword').value 
 
     const matchDisplay = document.getElementById('matchDisplay')
 
-    password != confirmPassord ? matchDisplay.innerText = 
-    'Passwords do not match' : verifyPassword()
-
+    password != confirmPassword ? matchDisplay.innerText = 'Passwords do not match' : verifyPassword()
 }
 
-    const createUser =(user,pass)=> {
-        userObj = {
-            id: user.length + 1,
-            username: user,
-            password: pass
-        }
-
-        users = [...users, userObj]
-
-        const display = document.getElementById('display')
-        display.innerText =`User ${userObj.username} created`
+const createUser =(user, pass)=> {
+    userObj = {
+        id: users.length + 1,
+        username: user,
+        password: pass
     }
+
+    console.log(userObj)
+    users = [...users, userObj]
+    console.log(users)
+    const display = document.getElementById('display')
+    display.innerText = `User ${userObj.username} created`
+}
 
 confirmBtn.addEventListener('click', (e)=> {
     e.preventDefault()
 
-    // verifyPassword()
     matchPasswords()
 })
